0f9dc0cc45da3e1f6b37103c7c143d89dc090e3f
Niels Keurentjes <niels.keurentjes@omines.com>
2015-11-04 13:14:15 +0100

    ApiGen bindings fixed.

--- bin/generate-api.sh
+++ bin/generate-api.sh
@@ -1,12 +1,13 @@
 mkdir -p _docs
 cd _docs
 git clone https://${GH_TOKEN}@github.com/omines/directadmin.git . > /dev/null
-git checkout -B gh-pages
+git checkout gh-pages
 rm -rf api
 
 # Generate Api
 wget http://www.apigen.org/apigen.phar
 php apigen.phar generate -s ../src -d api --template-theme "bootstrap"
+rm apigen.phar
 
 # Set identity
 git config --global user.email "travis@travis-ci.org"
@@ -14,6 +15,6 @@ git config --global user.name "Travis"
 
 # Push generated files
 git add .
-git commit -m "API updated"
+git commit -m "API documentation auto-updated"
 git push origin -fq > /dev/null
 
