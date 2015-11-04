9decd2398870c0c943b34c94fe7d163aae4da79d
Niels Keurentjes <niels.keurentjes@omines.com>
2015-11-04 12:06:21 +0100

    Another attempt at getting ApiGen to play nice.

--- bin/generate-api.sh
+++ bin/generate-api.sh
@@ -1,14 +1,11 @@
-# Get ApiGen.phar
-wget http://www.apigen.org/apigen.phar
-
-# Add branch
-mkdir -p ../gh-pages
-cd ../gh-pages
-git clone https://${GH_TOKEN}@github.com/omines/directadmin.git > /dev/null
+mkdir -p _docs
+cd _docs
+git clone https://${GH_TOKEN}@github.com/omines/directadmin.git . > /dev/null
 git checkout -B gh-pages
 rm -rf api
 
 # Generate Api
+wget http://www.apigen.org/apigen.phar
 php apigen.phar generate -s ../src -d api --template-theme "bootstrap"
 
 # Set identity
