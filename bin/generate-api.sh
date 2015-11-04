8bf5b9fe51879894bbeaf9ece79de6e9c4a4e016
Niels Keurentjes <niels.keurentjes@omines.com>
2015-11-04 11:52:42 +0100

    Adapted apigen to update and integrate with site.

--- bin/generate-api.sh
+++ bin/generate-api.sh
@@ -1,20 +1,22 @@
 # Get ApiGen.phar
 wget http://www.apigen.org/apigen.phar
 
-# Generate Api
-php apigen.phar generate -s src -d ../gh-pages/api
+# Add branch
+mkdir -p ../gh-pages
 cd ../gh-pages
+git clone https://${GH_TOKEN}@github.com/omines/directadmin.git > /dev/null
+git checkout -B gh-pages
+rm -rf api
+
+# Generate Api
+php apigen.phar generate -s ../src -d api --template-theme "bootstrap"
 
 # Set identity
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 
-# Add branch
-git init
-git remote add origin https://${GH_TOKEN}@github.com/omines/directadmin.git > /dev/null
-git checkout -B gh-pages
-
 # Push generated files
 git add .
 git commit -m "API updated"
-git push origin gh-pages -fq > /dev/null
+git push origin -fq > /dev/null
+
