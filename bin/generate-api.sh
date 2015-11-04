4dd043e3c1ee11a011366bc56277799141fa3b63
Niels Keurentjes <niels.keurentjes@omines.com>
2015-11-04 08:42:40 +0100

    Add API document generator.

---
+++ bin/generate-api.sh
@@ -0,0 +1,20 @@
+# Get ApiGen.phar
+wget http://www.apigen.org/apigen.phar
+
+# Generate Api
+php apigen.phar generate -s ../src -d ../../gh-pages/api
+cd ../../gh-pages
+
+# Set identity
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
+
+# Add branch
+git init
+git remote add origin https://${GH_TOKEN}@github.com/omines/directadmin.git > /dev/null
+git checkout -B gh-pages
+
+# Push generated files
+git add .
+git commit -m "API updated"
+git push origin gh-pages -fq > /dev/null
